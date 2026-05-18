c4438af4a0a203af53c99084c6e01ca35dc94441
riquemorato <112196094+riquemorato@users.noreply.github.com>
2026-05-18 17:01:04 -0300

    Initial commit

---
+++ src/main/java/com/automodelista/AutomodelistaApplication.java
@@ -0,0 +1,13 @@
+package com.automodelista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomodelistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomodelistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automodelista/AutomodelistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automodelista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomodelistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
