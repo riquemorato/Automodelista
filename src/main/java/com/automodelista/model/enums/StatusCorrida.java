ec217e6a9cd1d363ce61664adcc6a715aab28c80
riquemorato <112196094+riquemorato@users.noreply.github.com>
2026-06-12 10:04:44 -0300

    FIX: Reposicionamento do enum StatusCorrida no local certo

--- src/main/java/com/automodelista/model/enums/StatusCorrida.java
+++ src/main/java/com/automodelista/model/enums/StatusCorrida.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
  */
 
-package com.automodelista.model.estrategia;
+package com.automodelista.model.enums;
 
 /**
  *
