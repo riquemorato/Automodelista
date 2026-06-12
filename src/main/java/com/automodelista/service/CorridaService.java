94d07a3dc3b2d956453cfcb50936f3dc15dda437
riquemorato <112196094+riquemorato@users.noreply.github.com>
2026-06-12 11:55:46 -0300

    Implementacao: CorridaService + Fixes PilotoService

---
+++ src/main/java/com/automodelista/service/CorridaService.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.automodelista.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.automodelista.dao.CorridaDAO;
+import com.automodelista.dao.ParticipacaoCorridaDAO;
+import com.automodelista.dao.PilotoDAO;
+import com.automodelista.model.Corrida;
+import com.automodelista.model.ParticipacaoCorrida;
+import com.automodelista.model.enums.StatusCorrida;
+
+/**
+ *
+ * @author Henrique
+ */
+@Service
+public class CorridaService {
+    @Autowired CorridaDAO      corridaDAO;
+    @Autowired ParticipacaoCorridaDAO participacaoDAO;
+    @Autowired PilotoDAO       pilotoDAO;
+
+    public Corrida obterPorId(int id) { return corridaDAO.obterPorId(id); }
+
+    public List<ParticipacaoCorrida> obterParticipacoes(int corridaId) {
+        List<ParticipacaoCorrida> lista = participacaoDAO.obterPorCorrida(corridaId);
+        
+        for (ParticipacaoCorrida participacao : lista) {
+            participacao.setPiloto(pilotoDAO.obterPorId(participacao.getPilotoId()));
+        }
+
+        return lista;
+    }
+
+    public void inscreverPiloto(int pilotoId, int corridaId, String tipoEstrategia, String compound) {
+        participacaoDAO.inserir(new ParticipacaoCorrida(pilotoId, corridaId, tipoEstrategia, compound));
+    }
+
+    public void iniciarCorrida(int corridaId) {
+        Corrida corrida = corridaDAO.obterPorId(corridaId);
+        if (corrida.getStatus() != StatusCorrida.PENDENTE)
+            throw new IllegalStateException("Corrida já foi iniciada ou encerrada.");
+        corridaDAO.atualizarStatus(corridaId, StatusCorrida.EM_ANDAMENTO.name());
+    }
+}
--- src/main/java/com/automodelista/service/PilotoService.java
+++ src/main/java/com/automodelista/service/PilotoService.java
@@ -24,26 +24,32 @@ import com.automodelista.model.Piloto;
 public class PilotoService {
     @Autowired PilotoDAO pilotoDAO;
 
+    //POST - Insere um piloto novo
     public void inserir(Piloto piloto){
         pilotoDAO.inserir(piloto);
     }
 
+    //GET - Obtem um piloto cadastrado por ID
     public Piloto obterPorId(int id){
         return pilotoDAO.obterPorId(id);
     }
 
+    //GET - Obtem todos os pilotos
     public List<Piloto> obterTodos(){
         return pilotoDAO.obterTodos();
     }
 
+    //GET - obtem os pilotos por equipe
     public List<Piloto> obterPorEquipe(int id){
         return pilotoDAO.obterPorEquipe(id);
     }
 
+    //UPDATE - atualiza os dados de um piloto via ID
     public void atualizar(int id, Piloto p){
         pilotoDAO.atualizar(id, p);
     }
 
+    //DELETE - deleta um piloto via ID
     public void deletar(int id){
         pilotoDAO.deletar(id);
     }
