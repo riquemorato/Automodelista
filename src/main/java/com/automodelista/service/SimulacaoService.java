986d1740d17ae7c90212de359c93b08a2f523828
riquemorato <112196094+riquemorato@users.noreply.github.com>
2026-06-12 17:41:28 -0300

    FIX: move classe SimulacaoService para services

--- src/main/java/com/automodelista/service/SimulacaoService.java
+++ src/main/java/com/automodelista/service/SimulacaoService.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package com.automodelista.controller;
+package com.automodelista.service;
 
 //COMO FUNCIONA A SIMULAÇÃO: 
 
